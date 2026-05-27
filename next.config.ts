02d1f95b52203bc43a84a6ed8922a3d778f8d9d8
Suryansh Pratap Singh <iamsurya1804@gmail.com>
2026-05-27 16:09:33 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // This tells Next.js to keep the heavy database and encryption binaries outside the page bundle
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
-  },
+  serverExternalPackages: ["@prisma/client", "bcryptjs"],
 };
 
 export default nextConfig;
